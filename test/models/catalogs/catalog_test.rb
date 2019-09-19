7bd56f8d270d940095868c7c7d17ce9edb91745a
Eduardo Figarola <eduardofigarola@gmail.com>
2019-09-19 16:59:29 -0500

    Add tests

--- test/models/catalogs/catalog_test.rb
+++ test/models/catalogs/catalog_test.rb
@@ -4,8 +4,37 @@ require "test_helper"
 
 module Catalogs
   class CatalogTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    def test_it_creates_a_catalog
+      @catalog = Catalogs::Catalog.new(
+        model: "language",
+        field: "level",
+        label: "beginner",
+        value: "beginner"
+      )
+
+      assert @catalog.save
+    end
+
+    def test_it_fails_to_create_a_catalog_with_incomplete_params
+      @catalog = Catalogs::Catalog.new(
+        label: "beginner",
+        value: "beginner"
+      )
+
+      assert_not @catalog.save
+      assert_not @catalog.valid?
+    end
+
+    def test_it_fails_to_create_a_catalog_with_existing_label_value
+      @catalog = Catalogs::Catalog.new(
+        model: "skill",
+        field: "level",
+        label: "beginner",
+        value: "beginner"
+      )
+
+      assert_not @catalog.save
+      assert_not @catalog.valid?
+    end
   end
 end
