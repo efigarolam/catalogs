bc1fd2e644f4ca3e7c80811bb244d0778661eee0
Eduardo Figarola <eduardofigarola@gmail.com>
2019-09-19 16:28:41 -0500

    Add install generator

--- lib/generators/catalogs/install_generator.rb
+++ lib/generators/catalogs/install_generator.rb
@@ -2,6 +2,25 @@
 
 module Catalogs
   class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path("../templates", __FILE__)
+
     desc "Configure Application to use Catalogs Engine"
+
+    def include_engine_helper_methods
+      code = <<-RUBY
+  helper Catalogs::Engine.helpers
+      RUBY
+
+      inject_into_file(
+        "app/controllers/application_controller.rb",
+        after: "class ApplicationController < ActionController::Base\n"
+      ) do
+        code
+      end
+    end
+
+    def copy_locale_file
+      copy_file "catalogs.en.yml", "config/locales/catalogs.en.yml"
+    end
   end
 end
