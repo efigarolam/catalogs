69c2e08be5111ea9b59720392da650deccedbdf7
Eduardo Figarola <eduardofigarola@gmail.com>
2019-09-19 16:52:04 -0500

    Add uniqueness validation

--- app/models/catalogs/catalog.rb
+++ app/models/catalogs/catalog.rb
@@ -3,5 +3,6 @@
 module Catalogs
   class Catalog < ApplicationRecord
     validates_presence_of :model, :field, :label, :value
+    validates_uniqueness_of :label, :value, scope: %i[model field]
   end
 end
