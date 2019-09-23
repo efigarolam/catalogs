0839e69cc39e7ce4ae6f3c948839168547ef0bd9
Eduardo Figarola <eduardofigarola@gmail.com>
2019-09-23 12:03:12 -0500

    Fix validation error

--- app/models/catalogs/catalog.rb
+++ app/models/catalogs/catalog.rb
@@ -3,6 +3,6 @@
 module Catalogs
   class Catalog < ApplicationRecord
     validates_presence_of :model, :field, :value
-    validates_uniqueness_of :label, :value, scope: %i[model field]
+    validates_uniqueness_of :value, scope: %i[model field]
   end
 end
